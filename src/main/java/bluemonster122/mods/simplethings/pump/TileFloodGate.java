fd6b5028b7c099bdf82b241e7129175fcfffa020
blue <boo122333@gmail.com>
2017-05-08 21:34:16 +0100

    Making the Floodgate work

--- src/main/java/bluemonster122/mods/simplethings/pump/TileFloodGate.java
+++ src/main/java/bluemonster122/mods/simplethings/pump/TileFloodGate.java
@@ -3,6 +3,7 @@ package bluemonster122.mods.simplethings.pump;
 import bluemonster122.mods.simplethings.core.block.IHaveTank;
 import bluemonster122.mods.simplethings.core.block.TileST;
 import com.google.common.collect.ImmutableMap;
+import com.sun.istack.internal.NotNull;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.material.Material;
@@ -12,42 +13,43 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.fluids.FluidRegistry;
-import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.FluidTank;
-import net.minecraftforge.fluids.IFluidBlock;
+import net.minecraftforge.fluids.*;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fluids.capability.wrappers.BlockLiquidWrapper;
 import net.minecraftforge.fluids.capability.wrappers.BlockWrapper;
 import net.minecraftforge.fluids.capability.wrappers.FluidBlockWrapper;
 
+import javax.annotation.Nullable;
 import java.util.*;
 
 public class TileFloodGate extends TileST implements IHaveTank, ITickable {
     public FluidTank tank = createTank();
-    TreeMap<Integer, Deque<BlockPos>> layersToFill = new TreeMap<>();
-    Set<BlockPos> visited = new HashSet<>();
-    Deque<BlockPos> fluidBlocks = new LinkedList<>();
-    EnumSet<EnumFacing> blockFaces;
+    private TreeMap<Integer, Deque<BlockPos>> layersToFill = new TreeMap<>();
+    private Set<BlockPos> visited = new HashSet<>();
+    private Deque<BlockPos> fluidBlocks = new LinkedList<>();
+    private EnumSet<EnumFacing> blockedFaces;
+    private int ticks = 0;
 
     @Override
-    public Map<Capability, Capability> getCaps( ) {
+    public Map<Capability, Capability> getCaps() {
         return ImmutableMap.of(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY.cast((IFluidHandler) tank));
     }
 
     @Override
     public NBTTagCompound writeChild(NBTTagCompound tag) {
+        tag.setInteger("ticks", ticks);
         return tag;
     }
 
     @Override
     public NBTTagCompound readChild(NBTTagCompound tag) {
+        ticks = tag.getInteger("ticks");
         return tag;
     }
 
     @Override
-    public FluidTank getTank( ) {
+    public FluidTank getTank() {
         return tank;
     }
 
@@ -57,47 +59,106 @@ public class TileFloodGate extends TileST implements IHaveTank, ITickable {
     }
 
     @Override
-    public FluidTank createTank( ) {
-        return new FluidTank(1000);
+    public FluidTank createTank() {
+        return new FluidTank(Fluid.BUCKET_VOLUME);
     }
 
     @Override
-    public void update( ) {
+    public void update() {
         if (getWorld().isRemote) {
             updateClient();
         } else {
-//            if (getWorld().getTotalWorldTime() % 20 == 0)
+            tank.setFluid(new FluidStack(FluidRegistry.WATER, Fluid.BUCKET_VOLUME));
             updateServer();
-            // TODO: remove
-            tank.setFluid(new FluidStack(FluidRegistry.WATER, 1000));
         }
     }
 
-    private void updateServer( ) {
-        if (blockFaces == null) {
-            blockFaces = EnumSet.noneOf(EnumFacing.class);
+    private void updateServer() {
+        if (blockedFaces == null) {
+            blockedFaces = EnumSet.noneOf(EnumFacing.class);
             for (EnumFacing value : EnumFacing.VALUES) {
-                if (!isValidBlock(getPos().offset(value))){
-                    blockFaces.add(value);
+                if (!isValidBlock(getPos().offset(value))) {
+                    blockedFaces.add(value);
                 }
             }
         }
-        if (hasFluid()) {
-
+        FluidStack fluidOut = tank.drain(Fluid.BUCKET_VOLUME, false);
+        if (fluidOut != null) {
+            if (getWorld().provider.getDimension() == -1) {
+                tank.drain(Fluid.BUCKET_VOLUME, true);
+                return;
+            }
+//            if (ticks == 0) {
+//                ticks = 1200;
+                if (layersToFill.isEmpty())
+                    refreshQueues();
+//            }
+            BlockPos posToFill = getNextSpot(true);
+            if (posToFill != null && !placeFluidAt(posToFill)) {
+                ticks = 0;
+                return;
+            }
+            ticks--;
         }
     }
 
-    private void redoQueues( ) {
+    private boolean placeFluidAt(BlockPos fillPos) {
+        if (isValidBlock(fillPos)) {
+            boolean placed;
+            Block b = tank.getFluid().getFluid().getBlock();
+
+            if (b instanceof BlockFluidBase) {
+                BlockFluidBase blockFluid = (BlockFluidBase) b;
+                placed = getWorld().setBlockState(fillPos, blockFluid.getDefaultState(), 3);
+            } else {
+                placed = getWorld().setBlockState(fillPos, b.getDefaultState());
+            }
+
+            if (placed) {
+                addAdjacentToQueues(fillPos);
+                populateQueues();
+            }
+
+            return placed;
+        }
+        return false;
+    }
+
+    /**
+     * This will get the next block position that the flood gate should fill with the fluid contained within the tank.
+     *
+     * @param remove_value this should be true, if you want the method to poll the list, it will remove the value from being usable again.
+     * @return The next position that should be filled by the flodo gate.
+     */
+    @Nullable
+    private BlockPos getNextSpot(@NotNull boolean remove_value) {
+        if (layersToFill.isEmpty()) {
+            return null;
+        }
+
+        Deque<BlockPos> bottom_layer = layersToFill.firstEntry().getValue();
+
+        if (bottom_layer == null) {
+            return null;
+        }
+
+        if (bottom_layer.isEmpty()) {
+            layersToFill.pollFirstEntry();
+        }
+        return remove_value ? bottom_layer.pollFirst() : bottom_layer.peekFirst();
+    }
+
+    private void refreshQueues() {
         visited.clear();
         layersToFill.clear();
         fluidBlocks.clear();
 
-        queueAdjacent(pos);
+        addAdjacentToQueues(pos);
 
-        expandQueue();
+        populateQueues();
     }
 
-    private void expandQueue( ) {
+    private void populateQueues() {
         if (tank.getFluid() == null) {
             return;
         }
@@ -106,17 +167,17 @@ public class TileFloodGate extends TileST implements IHaveTank, ITickable {
             fluidBlocks = new LinkedList<>();
 
             for (BlockPos index : fluidsToExpand) {
-                queueAdjacent(index);
+                addAdjacentToQueues(index);
             }
         }
     }
 
-    private void queueAdjacent(BlockPos pos) {
+    private void addAdjacentToQueues(BlockPos pos) {
         if (tank.getFluid() == null) {
             return;
         }
         for (EnumFacing face : EnumFacing.VALUES) {
-            if (face != EnumFacing.UP && !blockFaces.contains(face)) {
+            if (face != EnumFacing.UP) {
                 queueForFilling(pos.offset(face));
             }
         }
@@ -130,35 +191,38 @@ public class TileFloodGate extends TileST implements IHaveTank, ITickable {
             if ((pos.getX() - getPos().getX()) * (pos.getX() - getPos().getX()) + (pos.getZ() - getPos().getZ()) * (pos.getZ() - getPos().getZ()) > 4096) {
                 return;
             }
-            if (isValidBlock(pos)) layersToFill.get(pos.getY()).addLast(pos);
+            IFluidHandler handler = FluidUtil.getFluidHandler(getWorld(), pos, null);
+            if (handler != null) {
+                FluidStack stack = handler.drain(Fluid.BUCKET_VOLUME, false);
+                if (stack != null) {
+                    if (stack.getFluid().equals(tank.getFluid().getFluid()))
+                        fluidBlocks.add(pos);
+                    if (stack.amount < Fluid.BUCKET_VOLUME) {
+                        addToFill(pos);
+                    }
+                }
+            }
+            if (isValidBlock(pos)) {
+                addToFill(pos);
+            }
         }
+    }
+
+    private void addToFill(BlockPos pos) {
+        layersToFill.computeIfAbsent(pos.getY(), k -> new LinkedList<>()).addLast(pos);
     }
 
     private boolean isValidBlock(BlockPos pos) {
         IBlockState state = getWorld().getBlockState(pos);
-        Block block = state.getBlock();
-        IFluidHandler handler;
-        if (block instanceof IFluidBlock) {
-            handler = new FluidBlockWrapper((IFluidBlock) block, getWorld(), pos);
-        } else if (block instanceof BlockLiquid) {
-            handler = new BlockLiquidWrapper((BlockLiquid) block, getWorld(), pos);
-        } else {
-            handler = new BlockWrapper(block, getWorld(), pos);
-        }
-        FluidStack drain = handler.drain(Integer.MAX_VALUE, false);
-        return !(drain != null && drain.amount >= 1000) && !(state.getMaterial().isSolid() || !state.getMaterial().isReplaceable() || state.getMaterial() != Material.AIR);
+        return (state.getMaterial().isReplaceable() && FluidUtil.getFluidHandler(getWorld(), pos, null) == null) || state.getMaterial() == Material.AIR;
     }
 
-    private boolean hasFluid( ) {
-        return tank.getFluid() != null && tank.getFluidAmount() >= 1000;
-    }
-
-    private void updateClient( ) {
+    private void updateClient() {
         /* NO OPERATION */
     }
 
     @Override
-    public void invalidate( ) {
+    public void invalidate() {
         super.invalidate();
         layersToFill.clear();
         fluidBlocks.clear();
