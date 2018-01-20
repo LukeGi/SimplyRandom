4612b225981193dceb3fe64594e6054d157db2bc
Blue_Monster122 <boo122333@gmail.com>
2018-01-20 22:36:36 +0000

    Fixes Recipes

--- src/main/java/bluemonster/simplyrandom/SimplyRandom.java
+++ src/main/java/bluemonster/simplyrandom/SimplyRandom.java
@@ -1,11 +1,16 @@
 package bluemonster.simplyrandom;
 
 import bluemonster.simplyrandom.proxy.IProxy;
+import bluemonster.simplyrandom.util.RecipeConverter;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
 
 import static bluemonster.simplyrandom.ModInfo.*;
 
@@ -17,6 +22,12 @@ public class SimplyRandom {
 
     @Instance(MOD_ID)
     public static SimplyRandom INSTANCE = null;
+    public static Logger logger;
+
+    @EventHandler
+    public void onPreInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
 
     @EventHandler
     public void onInit(FMLInitializationEvent event) {
